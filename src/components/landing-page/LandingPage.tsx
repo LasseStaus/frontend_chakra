291bd8318c3772b5d3961d1147d5c647149e4040
Lasse <l.stausgaard@gmail.com>
2022-05-10 15:10:59 +0200

    added header and footer to landingpage

--- src/components/landing-page/LandingPage.tsx
+++ src/components/landing-page/LandingPage.tsx
@@ -1,17 +1,21 @@
 import { Button, Container, Flex, Heading } from '@chakra-ui/react'
 import { useAuth } from '../../context/AuthContext'
 import AlertBox from '../alert/Alert'
+import Footer from '../footer/Footer'
+import Header from '../header/Header'
 import { ProfileBanner } from '../profile/ProfileBanner'
 
 function LandingPage() {
   const { user, login, logout, isLoading, authAlertActive, authAlert } = useAuth()
   console.log('from state', user?.access_token)
+
   return (
     <>
       {authAlertActive && <AlertBox status={authAlert} />}
-
+      <Header />
       <ProfileBanner />
       <Flex pb={10} flexDir='column'></Flex>
+      <Footer />
     </>
   )
 }
